ee49680525748bec96e304c32c7094a7efa016a9
Mike <zhengzhe94@gmail.com>
2019-11-05 15:05:47 +0800

    整理代码

--- src/Servers/Middleware/ExceptionSerializeMiddleware.php
+++ src/Servers/Middleware/ExceptionSerializeMiddleware.php
@@ -26,7 +26,7 @@ class ExceptionSerializeMiddleware extends MiddlewareAbstract
         try {
             $response = $next($request);
         } catch (\Exception $exception) {
-            // 先检查一下 message 是否可以 json_decode，避免多个服务相互调用出现多层嵌套
+            // 先检查一下 message 是否可以 json_decode，避免多个服务相互调用时出现多层嵌套
             if ($messageToArray = \json_decode($exception->getMessage())) {
                 throw new \Exception($exception->getMessage());
             }
