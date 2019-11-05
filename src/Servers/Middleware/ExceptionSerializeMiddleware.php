3ae9619b37a1425a1796e2adc6533611991a8022
Mike <zhengzhe94@gmail.com>
2019-11-05 15:05:24 +0800

    更新服务端异常序列化中间件，避免多个服务相互调用时出现多层嵌套

--- src/Servers/Middleware/ExceptionSerializeMiddleware.php
+++ src/Servers/Middleware/ExceptionSerializeMiddleware.php
@@ -26,6 +26,11 @@ class ExceptionSerializeMiddleware extends MiddlewareAbstract
         try {
             $response = $next($request);
         } catch (\Exception $exception) {
+            // 先检查一下 message 是否可以 json_decode，避免多个服务相互调用出现多层嵌套
+            if ($messageToArray = \json_decode($exception->getMessage())) {
+                throw new \Exception($exception->getMessage());
+            }
+
             throw new \Exception(\json_encode([
                 'exception' => get_class($exception),
                 'message' => $exception->getMessage(),
