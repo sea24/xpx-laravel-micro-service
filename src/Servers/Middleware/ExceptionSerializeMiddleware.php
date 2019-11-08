ebf4c186dc6ca98f9b81b7db3cf524663ec3bb01
Mike <zhengzhe94@gmail.com>
2019-11-08 20:52:44 +0800

    更新服务端异常序列化中间件，解决异常不能记录到日志的问题

--- src/Servers/Middleware/ExceptionSerializeMiddleware.php
+++ src/Servers/Middleware/ExceptionSerializeMiddleware.php
@@ -26,6 +26,10 @@ class ExceptionSerializeMiddleware extends MiddlewareAbstract
         try {
             $response = $next($request);
         } catch (\Exception $exception) {
+
+            // 将异常记录到日志
+            report($exception);
+
             // 先检查一下 message 是否可以 json_decode，避免多个服务相互调用时出现多层嵌套
             if ($messageToArray = \json_decode($exception->getMessage())) {
                 throw new \Exception($exception->getMessage());
